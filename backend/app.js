7774331ca4ac8ab4c351898391b3ef4e8629883b
emmanuel wiart <manu.wiart@gmail.com>
2022-07-12 16:23:18 +0200

    sequelize

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,42 @@
 const express = require('express');
-
 const app = express();
+const {Sequelize} = require('sequelize');
 
+const sequelize = new Sequelize('groupomania', 'root', '#Marie2117', {
+    dialect: 'mysql',
+    host: 'localhost'
+});
+async function test(){
+    try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    }
+}
+test();
+
+
+
+
+app.use((req, res, next) => {
+    console.log('requete reçu');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.json ({message: 'requete reçu'});
+    next();
+});
+
+app.use((req, res) => {
+    console.log('reponse envoyé avec succès');
+});
 
 
 module.exports = app;
