c8343026699ac3c007d5619746267e8037afac23
emmanuel wiart <manu.wiart@gmail.com>
2022-07-22 17:22:00 +0200

    middleware

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
-  
+
 const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize('groupomania', 'root', '#Marie2117', {
@@ -24,26 +26,31 @@ async function test(){
 }
 test();
 
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'publication créée'
+    });
+})
 
-
-
-app.use((req, res, next) => {
-    console.log('requete reçu');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json ({message: 'requete reçu'});
-    next();
-});
-
-app.use((req, res) => {
-    console.log('reponse envoyé avec succès');
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+    {
+        _id: 'oeihfzeoi',
+        title: 'Ma premiere publication',
+        description: 'Le contenu de ma 1ère publication',
+        imageUrl: '',
+        userId: 'qsomihvqios',
+    },
+    {
+        _id: 'oeihfzeomoihi',
+        title: 'Ma deuxième publication',
+        description: 'Le contenu de ma 2ème publication',
+        imageUrl: '',
+        userId: 'qsomihvqios',
+    },
+    ];
+    res.status(200).json(stuff);
 });
 
 
---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwentoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+
+        if (req.body.userId && req.body.userId !== userId){
+            return res.status(403).json("requête non autorisée");
+        }
+        else {
+            next();
+        }
+    }
+    catch {
+        res.status(401).json({ error: 'requête non authentifiée'});
+    }
+};
